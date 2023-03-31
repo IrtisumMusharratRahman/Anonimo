29d08d9302038e7d4b2a89cbbe03125f120bf15b
IrtisumMusharratRahman <rhythmfakir@gmail.com>
2023-03-31 22:52:41 +0800

    first commit

---
+++ src/main/java/com/project/server/Anonimo/AnonimoApplication.java
@@ -0,0 +1,13 @@
+package com.project.server.Anonimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonimoApplication.class, args);
+	}
+
+}
